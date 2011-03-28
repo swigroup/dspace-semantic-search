4c7ae03d4a204bd1af4b54b0b4aea19c4b85ddb9
borillo@gmail.com <borillo@gmail.com@30596628-216e-ac5e-b8a3-346198e9d920>
2011-03-28 16:57:30 +0000

    Removed optional import reference
    git-svn-id: https://dspace-semantic-search.googlecode.com/svn/trunk@106 30596628-216e-ac5e-b8a3-346198e9d920

--- dspace-semantic-api/src/main/java/gr/upatras/ceid/hpclab/owl/OWLDSpaceQueryManager.java
+++ dspace-semantic-api/src/main/java/gr/upatras/ceid/hpclab/owl/OWLDSpaceQueryManager.java
@@ -27,7 +27,6 @@ import org.semanticweb.owlapi.model.OWLEntity;
 import org.semanticweb.owlapi.model.OWLIndividual;
 import org.semanticweb.owlapi.model.OWLNamedIndividual;
 import org.semanticweb.owlapi.model.OWLObjectProperty;
-import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.reasoner.Node;
 import org.semanticweb.owlapi.vocab.PrefixOWLOntologyFormat;
 
