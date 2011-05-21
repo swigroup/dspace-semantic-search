704eb134ba9861830e1aa4b55a1a1c628ce6c7de
koutsomi <koutsomi@30596628-216e-ac5e-b8a3-346198e9d920>
2011-05-21 16:54:14 +0000

    - Add the ability to initialize the reasoner by precomputing inferences - Do not reset reasoner instance if not necessary
    git-svn-id: https://dspace-semantic-search.googlecode.com/svn/trunk@149 30596628-216e-ac5e-b8a3-346198e9d920

--- dspace-semantic-api/src/main/java/gr/upatras/ceid/hpclab/owl/SemanticUnit.java
+++ dspace-semantic-api/src/main/java/gr/upatras/ceid/hpclab/owl/SemanticUnit.java
@@ -15,117 +15,155 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
+import org.apache.log4j.Logger;
 import org.semanticweb.owlapi.apibinding.OWLManager;
 import org.semanticweb.owlapi.model.IRI;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 import org.semanticweb.owlapi.model.OWLOntologyManager;
+import org.semanticweb.owlapi.reasoner.InferenceType;
 import org.semanticweb.owlapi.reasoner.OWLReasoner;
 import org.semanticweb.owlapi.util.BidirectionalShortFormProvider;
 import org.semanticweb.owlapi.util.QNameShortFormProvider;
 import org.semanticweb.owlapi.util.ShortFormProvider;
 
-public class SemanticUnit
-{
-    private OWLOntologyManager manager;
-    private OWLOntology ontology;
-    private Set<OWLOntology> importsClosure;
-    private ShortFormProvider shortFormProvider;
-    private OWLReasoner reasoner;
-    private BidirectionalShortFormProvider bidirectionalShortFormProvider;
+public class SemanticUnit {
+	private OWLOntologyManager manager;
+	private OWLOntology ontology;
+	private Set<OWLOntology> importsClosure;
+	private ShortFormProvider shortFormProvider;
+	private OWLReasoner reasoner;
+	private BidirectionalShortFormProvider bidirectionalShortFormProvider;
 
-    private static Map<String, SemanticUnit> semanticUnitMap = new HashMap<String, SemanticUnit>();
+	private static final Logger log = Logger.getLogger(SemanticUnit.class);
 
-    private SemanticUnit(String url, SupportedReasoner supportedReasoner)
-            throws SemanticSearchException, InstantiationException, IllegalAccessException,
-            ClassNotFoundException
-    {
-        OWLDSpaceQueryManager owlQueryManager = setOntology(url);
+	private static Map<String, SemanticUnit> semanticUnitMap = new HashMap<String, SemanticUnit>();
 
-        importsClosure = manager.getImportsClosure(ontology);
+	private SemanticUnit(String url, SupportedReasoner supportedReasoner)
+			throws SemanticSearchException, InstantiationException,
+			IllegalAccessException, ClassNotFoundException {
+		OWLDSpaceQueryManager owlQueryManager = setOntology(url);
 
-        setReasoner(supportedReasoner);
-        setBidirectionalShortFormProvider(owlQueryManager);
-    }
+		importsClosure = manager.getImportsClosure(ontology);
 
-    public OWLDSpaceQueryManager setOntology(String url) throws SemanticSearchException
-    {
-        OWLDSpaceQueryManager owlQueryManager = new OWLDSpaceQueryManager(this);
+		setReasoner(supportedReasoner);
+		setBidirectionalShortFormProvider(owlQueryManager);
+	}
 
-        manager = OWLManager.createOWLOntologyManager();
+	public OWLDSpaceQueryManager setOntology(String url)
+			throws SemanticSearchException {
+		OWLDSpaceQueryManager owlQueryManager = new OWLDSpaceQueryManager(this);
 
-        try
-        {
-            ontology = manager.loadOntology(IRI.create(url));
-        }
-        catch (OWLOntologyCreationException exception)
-        {
+		manager = OWLManager.createOWLOntologyManager();
 
-            throw new SemanticSearchException("Could not load ontology", exception);
-        }
-        
-        return owlQueryManager;
-    }
+		try {
+			ontology = manager.loadOntology(IRI.create(url));
+		} catch (OWLOntologyCreationException exception) {
 
-    public void setBidirectionalShortFormProvider(OWLDSpaceQueryManager owlQueryManager)
-    {
-        Map<String, String> namespaces = owlQueryManager.buildNamespaceMapFromImportsClosure();
-        shortFormProvider = new QNameShortFormProvider(namespaces);
+			throw new SemanticSearchException("Could not load ontology",
+					exception);
+		}
 
-        bidirectionalShortFormProvider = BidirectionalShortFormProviderFactory.getInstance(this);
-    }
+		return owlQueryManager;
+	}
 
-    public void setReasoner(SupportedReasoner supportedReasoner) throws InstantiationException,
-            IllegalAccessException, ClassNotFoundException
-    {
-        OWLReasonerFactory owlReasonerFactory = (OWLReasonerFactory) Class.forName(
-                supportedReasoner.toString()).newInstance();
-        reasoner = owlReasonerFactory.getReasoner(ontology);
-    }
+	public void setBidirectionalShortFormProvider(
+			OWLDSpaceQueryManager owlQueryManager) {
+		Map<String, String> namespaces = owlQueryManager
+				.buildNamespaceMapFromImportsClosure();
+		shortFormProvider = new QNameShortFormProvider(namespaces);
 
-    public static SemanticUnit getInstance(String url, SupportedReasoner supportedReasoner)
-            throws SemanticSearchException, InstantiationException, IllegalAccessException,
-            ClassNotFoundException
-    {
-        if (!semanticUnitMap.containsKey(url))
-        {
-            semanticUnitMap.put(url, new SemanticUnit(url, supportedReasoner));
-        }
-        else
-        {
-            semanticUnitMap.get(url).setReasoner(supportedReasoner);
-        }
+		bidirectionalShortFormProvider = BidirectionalShortFormProviderFactory
+				.getInstance(this);
+	}
 
-        return semanticUnitMap.get(url);
-    }
+	public void setReasoner(SupportedReasoner supportedReasoner)
+			throws InstantiationException, IllegalAccessException,
+			ClassNotFoundException {
+		OWLReasonerFactory owlReasonerFactory = (OWLReasonerFactory) Class
+				.forName(supportedReasoner.toString()).newInstance();
 
-    public OWLOntologyManager getManager()
-    {
-        return manager;
-    }
+		reasoner = owlReasonerFactory.getReasoner(ontology);
 
-    public OWLOntology getOntology()
-    {
-        return ontology;
-    }
+	}
 
-    public Set<OWLOntology> getImportsClosure()
-    {
-        return importsClosure;
-    }
+	/** Initialize the  reasoner by precomputing inferences and flushing changes
+	 * if needed. 
+	 * @param reasoner
+	 * @return 0 if no initialization was needed.
+	 * 
+	 */
+	public int initializeReasoner(OWLReasoner reasoner) {
+		int status = 0;
+		// in case ontology has changed.
+		if (!reasoner.getPendingChanges().isEmpty()) {
+			reasoner.flush();
+			log.info("Reasoner flushed.");
+			status = 1;
+		}
+		// Initialize the reasoner: Precompute all inferences. Silenty ignore
+		// NPEs.
+		for (InferenceType t : InferenceType.values()) {
+			try {
+				if (!reasoner.isPrecomputed(t) && reasoner.getPrecomputableInferenceTypes().contains(t)) {
+					status = 2;
+					reasoner.precomputeInferences(t);
+				}
+			} catch (NullPointerException e) {
+				log.warn("NPE precomputing inference type: " + t);
+			}
+			log.debug(t + " is precomputed:" + reasoner.isPrecomputed(t));
+		}
+		return status;
+	}
 
-    public ShortFormProvider getShortFormProvider()
-    {
-        return shortFormProvider;
-    }
+	public static SemanticUnit getInstance(String url,
+			SupportedReasoner supportedReasoner)
+			throws SemanticSearchException, InstantiationException,
+			IllegalAccessException, ClassNotFoundException {
+		if (!semanticUnitMap.containsKey(url)) {
+			SemanticUnit su = new SemanticUnit(url, supportedReasoner);
+			semanticUnitMap.put(url, su);
+		}
+		// don't re-instatiate the reasoner if you don't have to
+		else if (!reasonerExists(semanticUnitMap.get(url), supportedReasoner)) {
+			semanticUnitMap.get(url).setReasoner(supportedReasoner);
+		}
 
-    public OWLReasoner getReasoner()
-    {
-        return reasoner;
-    }
+		return semanticUnitMap.get(url);
+	}
 
-    public BidirectionalShortFormProvider getBidirectionalShortFormProvider()
-    {
-        return bidirectionalShortFormProvider;
-    }
+	public OWLOntologyManager getManager() {
+		return manager;
+	}
+
+	public OWLOntology getOntology() {
+		return ontology;
+	}
+
+	public Set<OWLOntology> getImportsClosure() {
+		return importsClosure;
+	}
+
+	public ShortFormProvider getShortFormProvider() {
+		return shortFormProvider;
+	}
+
+	public OWLReasoner getReasoner() {
+		return reasoner;
+	}
+
+	public BidirectionalShortFormProvider getBidirectionalShortFormProvider() {
+		return bidirectionalShortFormProvider;
+	}
+
+	private static boolean reasonerExists(SemanticUnit su,
+			SupportedReasoner supportedReasoner) {
+
+		String currentReasoner = su.getReasoner().getReasonerName();
+
+		// at least works for Pellet!
+		return supportedReasoner.toString().contains(currentReasoner);
+
+	}
 }
